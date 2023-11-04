745e2dc266528e658a14a2d97f9c158d65f75944
Aahil13 <onyeanunaprince@example.com>
2023-11-04 23:22:30 +0100

    Refactored Cloudfront and S3 bucket configuration

--- Terraform/Cloudfront.tf
+++ Terraform/Cloudfront.tf
@@ -1,61 +1,61 @@
-# locals {
-#   s3_origin_id   = "aws_s3_bucket.resume_bucket.bucket"
-#   s3_domain_name = "${aws_s3_bucket.resume_bucket.bucket}.s3-website.us-east-1.amazonaws.com"
-# }
+locals {
+  s3_origin_id   = "aws_s3_bucket.resume_bucket.bucket"
+  s3_domain_name = "${aws_s3_bucket.resume_bucket.bucket}.s3-website.us-east-1.amazonaws.com"
+}
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = local.s3_domain_name
-#     origin_id   = local.s3_origin_id
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = local.s3_domain_name
+    origin_id   = local.s3_origin_id
 
-#     custom_origin_config {
-#       http_port              = "80"
-#       https_port             = "443"
-#       origin_protocol_policy = "http-only"
-#       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-#     }
-#   }
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
 
-#   enabled         = true
-#   is_ipv6_enabled = true
+  enabled         = true
+  is_ipv6_enabled = true
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = true
+    forwarded_values {
+      query_string = true
 
-#       cookies {
-#         forward = "all"
-#       }
-#     }
+      cookies {
+        forward = "all"
+      }
+    }
 
-#     viewer_protocol_policy = "https-only"
-#     min_ttl                = 0
-#     default_ttl            = 0
-#     max_ttl                = 0
-#   }
+    viewer_protocol_policy = "https-only"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
 
-#   price_class = "PriceClass_200"
+  price_class = "PriceClass_200"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-#   tags = {
-#     Environment = "production"
-#   }
+  tags = {
+    Environment = "production"
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
 
-# output "bucket_regional_domain_name" {
-#   value = aws_s3_bucket.resume_bucket.bucket_regional_domain_name
-# }
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.resume_bucket.bucket_regional_domain_name
+}
 
--- Terraform/S3_bucket.tf
+++ Terraform/S3_bucket.tf
@@ -1,76 +1,76 @@
-# ## Bucket for Resume
-# resource "aws_s3_bucket" "resume_bucket" {
-#   bucket = "aahil-resume-001"
+## Bucket for Resume
+resource "aws_s3_bucket" "resume_bucket" {
+  bucket = "aahil-resume-001"
 
-#   tags = {
-#     Name        = "resume_bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "resume_bucket"
+    Environment = "Dev"
+  }
+}
 
-# ## Bucket Public Access for Resume
-# resource "aws_s3_bucket_public_access_block" "resume_bucket_public_access_block" {
-#   bucket = aws_s3_bucket.resume_bucket.bucket
+## Bucket Public Access for Resume
+resource "aws_s3_bucket_public_access_block" "resume_bucket_public_access_block" {
+  bucket = aws_s3_bucket.resume_bucket.bucket
 
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-# ## Bucket Ownership for Resume
-# resource "aws_s3_bucket_ownership_controls" "resume_bucket_ownership_controls" {
-#   bucket = aws_s3_bucket.resume_bucket.bucket
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+## Bucket Ownership for Resume
+resource "aws_s3_bucket_ownership_controls" "resume_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.resume_bucket.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
-# ## Bucket ACL for Resume
-# resource "aws_s3_bucket_acl" "my-static-website" {
-#   depends_on = [
-#     aws_s3_bucket_ownership_controls.resume_bucket_ownership_controls,
-#     aws_s3_bucket_public_access_block.resume_bucket_public_access_block,
-#   ]
+## Bucket ACL for Resume
+resource "aws_s3_bucket_acl" "my-static-website" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.resume_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.resume_bucket_public_access_block,
+  ]
 
-#   bucket = aws_s3_bucket.resume_bucket.bucket
-#   acl    = "public-read"
-# }
+  bucket = aws_s3_bucket.resume_bucket.bucket
+  acl    = "public-read"
+}
 
-# ## Bucket Policy for Resume
-# resource "aws_s3_bucket_policy" "resume_bucket_policy" {
-#   bucket = aws_s3_bucket.resume_bucket.bucket
+## Bucket Policy for Resume
+resource "aws_s3_bucket_policy" "resume_bucket_policy" {
+  bucket = aws_s3_bucket.resume_bucket.bucket
 
-#   policy = <<POLICY
-# {
-#   "Id": "Policy",
-#   "Statement": [
-#     {
-#       "Action": [
-#         "s3:GetObject"
-#       ],
-#       "Effect": "Allow",
-#       "Resource": "arn:aws:s3:::${aws_s3_bucket.resume_bucket.bucket}/*",
-#       "Principal": {
-#         "AWS": [
-#           "*"
-#         ]
-#       }
-#     }
-#   ]
-# }
-# POLICY
-# }
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.resume_bucket.bucket}/*",
+      "Principal": {
+        "AWS": [
+          "*"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+}
 
-# ## Bucket Static Site Configuration for Resume
-# resource "aws_s3_bucket_website_configuration" "resume_static_website" {
-#   bucket = aws_s3_bucket.resume_bucket.bucket
+## Bucket Static Site Configuration for Resume
+resource "aws_s3_bucket_website_configuration" "resume_static_website" {
+  bucket = aws_s3_bucket.resume_bucket.bucket
 
-#   index_document {
-#     suffix = "index.html"
-#   }
-# }
+  index_document {
+    suffix = "index.html"
+  }
+}
 
-# output "website_url" {
-#   value = "http://${aws_s3_bucket.resume_bucket.bucket}.s3-website.us-east-1.amazonaws.com"
-# }
+output "website_url" {
+  value = "http://${aws_s3_bucket.resume_bucket.bucket}.s3-website.us-east-1.amazonaws.com"
+}
