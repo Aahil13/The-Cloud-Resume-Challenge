a50483ea62fbf42c12c048e8b7b50821ebc6669b
Aahil13 <onyeanunaprince@example.com>
2023-11-04 05:30:56 +0100

    Add AWS API Gateway resources for resume counter

---
+++ Terraform/API_GATEWAY.tf
@@ -0,0 +1,66 @@
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "resume_counter_api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["http://127.0.0.1:5500"]
+    allow_methods = ["GET"]
+    allow_headers = ["Content-Type"]
+    max_age       = 300
+  }
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "dev"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "resume_counter" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.resume_counter.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "resume_counter" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.resume_counter.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.resume_counter.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
